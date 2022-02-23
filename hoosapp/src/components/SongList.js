614d47d1afb5be9280e896a6b1bb5bfd3acc5694
KhubaibAhmadw2 <khubaibahmadw2@gmail.com>
2022-02-23 18:03:37 +0500

    hooksapp

---
+++ hoosapp/src/components/SongList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SongList = () => {
+    return ( 
+        <div className="song-list">
+            <ul>
+                <li>Fearless</li>
+                <li>We not talk Anymore</li>
+
+            </ul>
+        </div>
+     );
+}
+ 
+export default SongList;
